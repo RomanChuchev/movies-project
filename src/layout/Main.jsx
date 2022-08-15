febf7ebcd713946606bac544ce9b9199160e4f51
Roman Chuchev <romanok20@gmail.com>
2022-08-15 18:24:09 +0300

    Refactoring Main use hooks

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,43 +1,36 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Movies } from "../components/Movies";
 import Search from "../components/Search";
 import Spinner from "../components/spinner";
 
-export default class Main extends Component {
-  constructor() {
-    super();
+const Main = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    this.state = {
-      movies: [],
-      loading: false,
-    };
-
-    this.searchMovies = this.searchMovies.bind(this);
-  }
-
-  searchMovies(str, type = "all") {
-    this.setState({ loading: true });
+  const searchMovies = (str, type = "all") => {
+    setLoading(true);
     fetch(
       `https://www.omdbapi.com/?apikey=347de6a3&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .then((data) => {
+        setLoading(false);
+        setMovies(data.Search);
+      })
       .catch((err) => {
         console.log(err);
-        this.setState({ loading: false });
+        setLoading(false);
       });
-  }
+  };
 
-  render() {
-    const { movies, loading } = this.state;
+  return (
+    <main className="container content">
+      <Search searchMovies={searchMovies} />
+      {loading ? <Spinner /> : <Movies movies={movies} />}
+    </main>
+  );
+};
 
-    return (
-      <main className="container content">
-        <Search searchMovies={this.searchMovies} />
-        {loading ? <Spinner /> : <Movies movies={movies} />}
-      </main>
-    );
-  }
-}
+export default Main;
